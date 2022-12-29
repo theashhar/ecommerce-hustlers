567a227f11b707e52af0a2c5c655d5d96bfe4a77
Ashhar Ali Ahmed <ashhar1111981@gmail.com>
2022-12-29 17:48:56 +0530

    small ui change

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -9,7 +9,7 @@ export default function Cart() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 pt-16 pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Shopping Cart</h1>
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Shoppouting Cart</h1>
         <form className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
           <section aria-labelledby="cart-heading" className="lg:col-span-7">
             <h2 id="cart-heading" className="sr-only">
